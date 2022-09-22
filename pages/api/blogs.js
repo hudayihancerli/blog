c6463f644bc74426957fe5a4ff59c8fd6495a6ef
Mahmut hüdayi hançerli <m.hudayihancerli@gmail.com>
2022-09-23 00:18:45 +0300

    name change

--- pages/api/blogs.js
+++ pages/api/blogs.js
@@ -1,5 +1,6 @@
-import { getBlogs } from "../../src/blogsDescription"
+import  getBlogs  from "../../src/blogsDescription"
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default async(req, res) => {
     const blogs = await getBlogs();
     res.json({ blogs })
